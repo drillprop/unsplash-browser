7647cbfef3efddc19632cb8a7fb76b5f7b0f1a2e
drillprop <drylbartosz@gmail.com>
2020-11-06 00:05:59 +0100

    Create search endpoint

--- server.ts
+++ server.ts
@@ -1,17 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-
 app.post('/autocomplete/:searchterm', async (req, res) => {
   try {
+    const { searchterm } = req.params;
     const response = await fetch(
-      `https://unsplash.com/nautocomplete/${req.params.searchterm}`
+      `https://unsplash.com/nautocomplete/${searchterm}`
     );
     const json = await response.json();
     res.send(json);
@@ -19,6 +21,21 @@ app.post('/autocomplete/:searchterm', async (req, res) => {
     res.status(400).send('Something went wrong');
   }
 });
+
+app.post('/search/:searchterm', async (req, res) => {
+  try {
+    const { searchterm } = req.params;
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${searchterm}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    );
+    const json = await response.json();
+    res.send(json);
+  } catch (error) {
+    res.status(400).send('Something went wrong');
+  }
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
