14888a2c051a31bbd0d258037b1b8396d8467bc0
drillprop <drylbartosz@gmail.com>
2020-11-08 23:25:59 +0100

    Add LoadingSpinner and close button to ImageModal

--- client/src/components/ImageModal/ImageModal.tsx
+++ client/src/components/ImageModal/ImageModal.tsx
@@ -7,6 +7,7 @@ import styles from './ImageModal.module.scss';
 
 import { ReactComponent as PinIcon } from 'assets/icons/pin-icon.svg';
 import clsx from 'clsx';
+import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner';
 
 interface ParamTypes {
   id: string;
@@ -16,38 +17,56 @@ const ImageModal = () => {
   const history = useHistory();
   const [zoom, setZoom] = useState(false);
   const { id } = useParams<ParamTypes>();
-  const { data } = useQuery<UnsplashPhotoResponse>(['photo', id], fetcher);
+  const { data, isLoading } = useQuery<UnsplashPhotoResponse>(
+    ['photo', id],
+    fetcher
+  );
 
-  const back = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const back = (
+    e: React.MouseEvent<HTMLDivElement | HTMLButtonElement, MouseEvent>
+  ) => {
     if (e.target === e.currentTarget) {
       history.goBack();
     }
   };
-  if (!data) return null;
 
   return (
     <div className={styles.modalShadow} onClick={back}>
       <div className={styles.modalWrapper}>
-        <header className={styles.modalHeader}>
-          <img
-            className={styles.authorImage}
-            src={data.user.profile_image.small}
-            alt={data.user.name}
-          />
-          <h2 className={styles.authorName}>{data.user.name}</h2>
-          <p className={styles.authorNick}>@{data.user.username}</p>
-        </header>
-        <figure className={styles.figureWrapper}>
-          <img
-            className={clsx(styles.figureImage, { [styles.zoomed]: zoom })}
-            src={data.urls.regular}
-            onClick={() => setZoom(!zoom)}
-            alt={data.alt_description || `unsplash photo by ${data.user.name}`}
-          />
-          <figcaption className={styles.figureLocation}>
-            {data.location.title && <PinIcon />} {data.location.title}
-          </figcaption>
-        </figure>
+        <button
+          className={styles.closeModalButton}
+          onClick={back}
+          title='close modal'
+        >
+          &times;
+        </button>
+        {isLoading && <LoadingSpinner />}
+        {!isLoading && data && (
+          <>
+            <header className={styles.modalHeader}>
+              <img
+                className={styles.authorImage}
+                src={data.user.profile_image.small}
+                alt={data.user.name}
+              />
+              <h2 className={styles.authorName}>{data.user.name}</h2>
+              <p className={styles.authorNick}>@{data.user.username}</p>
+            </header>
+            <figure className={styles.figureWrapper}>
+              <img
+                className={clsx(styles.figureImage, { [styles.zoomed]: zoom })}
+                src={data.urls.regular}
+                onClick={() => setZoom(!zoom)}
+                alt={
+                  data.alt_description || `unsplash photo by ${data.user.name}`
+                }
+              />
+              <figcaption className={styles.figureLocation}>
+                {data.location.title && <PinIcon />} {data.location.title}
+              </figcaption>
+            </figure>
+          </>
+        )}
       </div>
     </div>
   );
