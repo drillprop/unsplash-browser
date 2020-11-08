672e3017beb2b9c8ba21dc4502377c6656b97b11
drillprop <drylbartosz@gmail.com>
2020-11-08 23:44:33 +0100

    Setup hash placeholders for images

--- client/src/components/Image/Image.tsx
+++ client/src/components/Image/Image.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { useBlurhash } from '../../hooks/useBlurhash';
 import { Result } from '../../types/api';
 import styles from './Image.module.scss';
 
@@ -8,9 +9,25 @@ interface Props {
 }
 
 const Image = ({ photo }: Props) => {
+  const [isImgLoaded, setIsImgLoaded] = useState(false);
+  const hashUrl = useBlurhash(isImgLoaded ? null : photo.blur_hash);
+  const [height, setHeight] = useState(0);
   const location = useLocation();
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const divider = photo.width / ref.current.clientWidth;
+    setHeight(photo.height / divider);
+  }, [photo.height, photo.width]);
+
   return (
-    <div>
+    <div
+      ref={ref}
+      style={{
+        height,
+      }}
+    >
       <Link
         key={photo.id}
         to={{
@@ -19,6 +36,14 @@ const Image = ({ photo }: Props) => {
         }}
       >
         <img
+          onLoad={() => setIsImgLoaded(true)}
+          width={ref.current?.clientWidth}
+          height={height}
+          style={{
+            background: `url(${hashUrl})`,
+            backgroundSize: `${ref.current?.clientWidth}px ${height}px`,
+            backgroundRepeat: 'no-repeat',
+          }}
           src={photo.urls.small}
           alt={photo.alt_description || 'photography'}
           loading='lazy'
